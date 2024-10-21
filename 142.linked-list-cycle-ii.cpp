e3e4d8510d30a35b7f51f12452769959b5bea6a4
AlanYuc <alan.yuc@outlook.com>
2024-10-21 17:35:06 +0800

    Create 142.linked-list-cycle-ii.cpp

---
+++ 142.linked-list-cycle-ii.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ * @lcpr version=30204
+ *
+ * [142] 环形链表 II
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (fast == slow)
+            {
+                ListNode *index1 = head;
+                ListNode *index2 = fast;
+                while (index1 != index2)
+                {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index1;
+            }
+        }
+        return NULL;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [3,2,0,-4]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n-1\n
+// @lcpr case=end
+
+ */
