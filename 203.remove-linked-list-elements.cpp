18a707aa101f81bfa83c91fd5005f99593985123
AlanYuc <alan.yuc@outlook.com>
2024-10-21 11:23:16 +0800

    Create 203.remove-linked-list-elements.cpp

---
+++ 203.remove-linked-list-elements.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=cpp
+ * @lcpr version=30204
+ *
+ * [203] 移除链表元素
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        ListNode *dummyNode = new ListNode();
+        dummyNode->next = head;
+        ListNode *cur = dummyNode;
+        while (cur->next != NULL)
+        {
+            if (cur->next->val == val)
+            {
+                ListNode *temp = cur->next;
+                cur->next = cur->next->next;
+                delete temp;
+            }
+            else
+            {
+                cur = cur->next;
+            }
+        }
+        return dummyNode->next;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,6,3,4,5,6]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [7,7,7,7]\n7\n
+// @lcpr case=end
+
+ */
