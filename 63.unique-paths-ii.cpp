0eb83eaf88475b6bd7e3c2f56b3d39ce7ebd8b70
AlanYuc <alan.yuc@outlook.com>
2024-10-23 22:55:39 +0800

    Create 63.unique-paths-ii.cpp

---
+++ 63.unique-paths-ii.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ * @lcpr version=30204
+ *
+ * [63] 不同路径 II
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution
+{
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        for (int i = 0; i < m; i++)
+        {
+            if (obstacleGrid[i][0] == 1)
+                break;
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            if (obstacleGrid[0][j] == 1)
+                break;
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (obstacleGrid[i][j] == 1)
+                    continue;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [[0,0,0],[0,1,0],[0,0,0]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,1],[0,0]]\n
+// @lcpr case=end
+
+ */
