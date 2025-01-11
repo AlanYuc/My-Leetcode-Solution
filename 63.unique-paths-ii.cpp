2a16d19e48d53a64a179a4473243a9ce8ab66118
AlanYuc <alan.yuc@outlook.com>
2025-01-11 13:40:40 +0800

    Update 63.unique-paths-ii.cpp

--- 63.unique-paths-ii.cpp
+++ 63.unique-paths-ii.cpp
@@ -32,18 +32,24 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m, vector<int>(n));
-        for (int i = 0; i < m; i++)
-        {
-            if (obstacleGrid[i][0] == 1)
-                break;
+        // for (int i = 0; i < m; i++)
+        // {
+        //     if (obstacleGrid[i][0] == 1)
+        //         break;
+        //     dp[i][0] = 1;
+        // }
+        // for (int j = 0; j < n; j++)
+        // {
+        //     if (obstacleGrid[0][j] == 1)
+        //         break;
+        //     dp[0][j] = 1;
+        // }
+        
+        //初始化的语句简化
+        for(int i = 0;i<m && obstacleGrid[i][0] == 0;i++)
             dp[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++)
-        {
-            if (obstacleGrid[0][j] == 1)
-                break;
+        for(int j = 0;j<n && obstacleGrid[0][j] == 0;j++)
             dp[0][j] = 1;
-        }
 
         for (int i = 1; i < m; i++)
         {
