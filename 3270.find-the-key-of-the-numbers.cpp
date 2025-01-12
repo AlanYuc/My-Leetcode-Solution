f52cd7718f8496abf2219dac18956d772c38a888
AlanYuc <alan.yuc@outlook.com>
2025-01-12 11:13:23 +0800

    Update 3270.find-the-key-of-the-numbers.cpp

--- 3270.find-the-key-of-the-numbers.cpp
+++ 3270.find-the-key-of-the-numbers.cpp
@@ -27,32 +27,49 @@ using namespace std;
 class Solution
 {
 public:
-    int minKey(int num1, int num2, int num3)
-    {
-        int min1 = num1 < num2 ? num1 : num2;
-        int min2 = min1 < num3 ? min1 : num3;
-        return min2;
-    }
+    // int minKey(int num1, int num2, int num3)
+    // {
+    //     int min1 = num1 < num2 ? num1 : num2;
+    //     int min2 = min1 < num3 ? min1 : num3;
+    //     return min2;
+    // }
 
+    // int generateKey(int num1, int num2, int num3)
+    // {
+    //     vector<vector<int>> ans(3, vector<int>(4));
+    //     for (int i = 3; i >= 0; i--)
+    //     {
+    //         ans[0][i] = num1 % 10;
+    //         num1 /= 10;
+
+    //         ans[1][i] = num2 % 10;
+    //         num2 /= 10;
+
+    //         ans[2][i] = num3 % 10;
+    //         num3 /= 10;
+    //     }
+
+    //     int key = 0;
+    //     for (int i = 0; i < 4; i++)
+    //     {
+    //         key = key * 10 + minKey(ans[0][i], ans[1][i], ans[2][i]);
+    //     }
+    //     return key;
+    // }
+
+    // 继续优化
     int generateKey(int num1, int num2, int num3)
     {
-        vector<vector<int>> ans(3, vector<int>(4));
-        for (int i = 3; i >= 0; i--)
-        {
-            ans[0][i] = num1 % 10;
-            num1 /= 10;
-
-            ans[1][i] = num2 % 10;
-            num2 /= 10;
-
-            ans[2][i] = num3 % 10;
-            num3 /= 10;
-        }
-
         int key = 0;
-        for (int i = 0; i < 4; i++)
+        int p = 1;
+        while (num1 && num2 && num3)
         {
-            key = key * 10 + minKey(ans[0][i], ans[1][i], ans[2][i]);
+            // 注意min函数的用法，{}可以进行多值比较
+            key += p * (min({num1 % 10, num2 % 10, num3 % 10}));
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+            p *= 10;
         }
         return key;
     }
