9e7c5da7eb009592ee6bd1be595491f96712ca8b
AlanYuc <alan.yuc@outlook.com>
2025-01-13 15:52:14 +0800

    Create 416.partition-equal-subset-sum.cpp

---
+++ 416.partition-equal-subset-sum.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=416 lang=cpp
+ * @lcpr version=30204
+ *
+ * [416] 分割等和子集
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution
+{
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1)
+        {
+            return false;
+        }
+
+        sum /= 2;
+        vector<int> dp(20001, 0);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = sum; j >= nums[i]; j--)
+            {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        if (dp[sum] == sum)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,5,11,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,5]\n
+// @lcpr case=end
+
+ */
