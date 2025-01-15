3c6c2e70eef6a74eae6649013456a141c5cf2d4a
AlanYuc <alan.yuc@outlook.com>
2025-01-15 11:17:11 +0800

    Update 3066.minimum-operations-to-exceed-threshold-value-ii.cpp

--- 3066.minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3066.minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -29,43 +29,59 @@ class Solution
 public:
     int minOperations(vector<int> &nums, int k)
     {
-        //最终超时
-        //错误记录：1、long long，需要考虑数据超范围的情况
-        //2、需要考虑开始就符合条件，操作数为0的情况
-        //3、中间用冒泡的方式保证数组的顺序，但时间复杂度o（n2），最终有个测试用例超时
-        sort(nums.begin(), nums.end());
-        vector<long long> vec;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            vec.push_back(nums[i]);
-        }
+        // 最终超时
+        // 错误记录：1、long long，需要考虑数据超范围的情况
+        // 2、需要考虑开始就符合条件，操作数为0的情况
+        // 3、中间用冒泡的方式保证数组的顺序，但时间复杂度o（n2），最终有个测试用例超时
+        //  sort(nums.begin(), nums.end());
+        //  vector<long long> vec;
+        //  for (int i = 0; i < nums.size(); i++)
+        //  {
+        //      vec.push_back(nums[i]);
+        //  }
+        //  int op = 0;
+
+        // if (vec[0] >= k)
+        //     return op;
+
+        // for (int i = 0; i < vec.size() - 1; i++)
+        // {
+        //     long long temp = min(vec[i], vec[i + 1]) * 2 + max(vec[i], vec[i + 1]);
+        //     vec[i] = 0;
+        //     vec[i + 1] = temp;
+        //     for (int j = i + 1; j < vec.size() - 1; j++)
+        //     {
+        //         if (vec[j] > vec[j + 1])
+        //         {
+        //             int val = vec[j + 1];
+        //             vec[j + 1] = vec[j];
+        //             vec[j] = val;
+        //         }
+        //     }
+        //     if (vec[i + 1] >= k)
+        //     {
+        //         return i + 1;
+        //     }
+        //     else
+        //     {
+        //         op++;
+        //     }
+        // }
+        // return op;
+
+        // 更新，用优先级队列，底层使用的堆排
+        priority_queue<long long, vector<long long>, greater<long long>> pq(nums.begin(), nums.end());
         int op = 0;
-
-        if (vec[0] >= k)
-            return op;
-
-        for (int i = 0; i < vec.size() - 1; i++)
+        while (pq.top() < k)
         {
-            long long temp = min(vec[i], vec[i + 1]) * 2 + max(vec[i], vec[i + 1]);
-            vec[i] = 0;
-            vec[i + 1] = temp;
-            for (int j = i + 1; j < vec.size() - 1; j++)
-            {
-                if (vec[j] > vec[j + 1])
-                {
-                    int val = vec[j + 1];
-                    vec[j + 1] = vec[j];
-                    vec[j] = val;
-                }
-            }
-            if (vec[i + 1] >= k)
-            {
-                return i + 1;
-            }
-            else
-            {
-                op++;
-            }
+            long long a = pq.top();
+            pq.pop();
+            long long b = pq.top();
+            pq.pop();
+
+            long long c = min(a, b) * 2 + max(a, b);
+            pq.push(c);
+            op++;
         }
         return op;
     }
