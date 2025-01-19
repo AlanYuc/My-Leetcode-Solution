998975bdd20982292757245e2d6e52d0876ecc54
AlanYuc <alan.yuc@outlook.com>
2025-01-19 21:59:01 +0800

    Update 377.combination-sum-iv.cpp

--- 377.combination-sum-iv.cpp
+++ 377.combination-sum-iv.cpp
@@ -31,14 +31,12 @@ public:
     {
         vector<unsigned int> dp(target + 1);
 
+        //初始化是1，但没有明确的意义，只是确保后续可以迭代
         dp[0] = 1;
-        
-        // for (int i = 1; i <= target; i++)
-        // {
-        //     if (i % nums[0] == 0)
-        //         dp[i] = 1;
-        // }
 
+        //题目要求是求所有组合，所以对遍历顺序有要求
+        //先遍历背包容量再遍历物品--求所有排列组合
+        //先遍历物品再遍历背包容量--只求组合（不要求顺序）
         for (int i = 0; i <= target; i++)
         {
             for (int j = 0; j < nums.size(); j++)
